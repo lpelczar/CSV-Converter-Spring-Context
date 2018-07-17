3a8799303c570da3a86b9172f9f48121ac50e3d9
lpelczar <lukepelczar@gmail.com>
2018-07-17 16:46:30 +0200

    Implement handling program arguments

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -6,7 +6,34 @@ public class ConverterApplication {
 
         if (args.length == 0) {
             System.out.println("No input file defined");
+            return;
         }
+        handleInputArguments(args);
+    }
 
+    private static void handleInputArguments(String[] args) {
+        if (args.length == 1) {
+            String filePath = args[0];
+            // Check if path is correct and start
+        } else if (args.length == 2) {
+
+            OutputFormat outputFormat = null;
+            for (OutputFormat format : OutputFormat.values()) {
+                if (format.toString().equals(args[0].toUpperCase())) {
+                    outputFormat = format;
+                }
+            }
+
+            if (outputFormat == null) {
+                System.out.println("Invalid format");
+                return;
+            }
+
+            String filePath = args[1];
+            // Check if path is correct and start
+
+        } else {
+            System.out.println("Invalid arguments");
+        }
     }
 }
