90fa4fbf0d4dd9cee9aad4f88f232c5321c7403a
lpelczar <lukepelczar@gmail.com>
2018-07-17 13:21:31 +0200

    Handle no input file

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -3,6 +3,10 @@ package com.codecool.scc;
 public class ConverterApplication {
 
     public static void main(String[] args) {
-        
+
+        if (args.length == 0) {
+            System.out.println("No input file defined");
+        }
+
     }
 }
