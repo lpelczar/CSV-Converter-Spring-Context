8c25dfd0b759e4701b07e3c5ea84925c085fe95f
lpelczar <lukepelczar@gmail.com>
2018-07-18 12:44:29 +0200

    Implement conversion to xml

--- src/main/java/com/codecool/scc/output/JsonOutputFormatter.java
+++ src/main/java/com/codecool/scc/output/JsonOutputFormatter.java
@@ -7,11 +7,12 @@ import java.util.List;
 import java.util.Map;
 
 public class JsonOutputFormatter implements OutputFormatter {
+
     public void printToConsole(List<Map<?, ?>> data) {
 
         String json = "";
-
         ObjectMapper mapper = new ObjectMapper();
+
         try {
             json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
         } catch (IOException e) {
--- src/main/java/com/codecool/scc/output/XmlOutputFormatter.java
+++ src/main/java/com/codecool/scc/output/XmlOutputFormatter.java
@@ -1,10 +1,24 @@
 package com.codecool.scc.output;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 import java.util.List;
 import java.util.Map;
 
 public class XmlOutputFormatter implements OutputFormatter {
+
     public void printToConsole(List<Map<?, ?>> data) {
-        System.out.println("XML");
+
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = "";
+
+        try {
+            xml = xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(xml);
     }
 }
