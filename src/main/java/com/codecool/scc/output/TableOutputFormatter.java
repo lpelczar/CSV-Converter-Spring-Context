9672a1581b5279bb5a9216eebe59e9c06b798f52
lpelczar <lukepelczar@gmail.com>
2018-07-18 14:21:02 +0200

    Implement table printing

--- src/main/java/com/codecool/scc/output/TableOutputFormatter.java
+++ src/main/java/com/codecool/scc/output/TableOutputFormatter.java
@@ -2,9 +2,16 @@ package com.codecool.scc.output;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class TableOutputFormatter implements OutputFormatter {
     public void printToConsole(List<Map<?, ?>> data) {
-        System.out.println("TABLE");
+
+        for (Map<?,?> item : data) {
+            for (Entry<?,?> entry : item.entrySet()) {
+                System.out.print(entry.getKey() + " : " + entry.getValue() + " | ");
+            }
+            System.out.println();
+        }
     }
 }
